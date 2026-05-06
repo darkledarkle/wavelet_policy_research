243fd372703550ad55df96a53f7dd9ce0b4001c6
Iggy <dagnino.iggy@gmail.com>
2026-05-06 00:58:18 -0700

    Kitchen

--- diffusion_policy/workspace/train_diffusion_transformer_lowdim_workspace.py
+++ diffusion_policy/workspace/train_diffusion_transformer_lowdim_workspace.py
@@ -22,7 +22,7 @@ import shutil
 
 from diffusion_policy.common.pytorch_util import dict_apply, optimizer_to
 from diffusion_policy.workspace.base_workspace import BaseWorkspace
-from diffusion_policy.policy.diffusion_transformer_lowdim_policy import DiffusionTransformerLowdimPolicy
+from diffusion_policy.policy.diffusion_wavelet_lowdim_policy import DiffusionWaveletLowdimPolicy
 from diffusion_policy.dataset.base_dataset import BaseLowdimDataset
 from diffusion_policy.env_runner.base_lowdim_runner import BaseLowdimRunner
 from diffusion_policy.common.checkpoint_util import TopKCheckpointManager
